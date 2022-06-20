8110e4350ae59475c476c8d23cba35cd74860683
simo333 <szymon.333@wp.pl>
2022-06-20 22:38:52 +0200

    Uploading local project files

---
+++ src/test/java/com/simo333/springinaction/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simo333.springinaction.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
