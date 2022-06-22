1cde7923f40113a0e5c8ff92f345ebcf51a54510
simo333 <szymon.333@wp.pl>
2022-06-22 18:47:40 +0200

    Added registration and log in/out

--- src/main/java/com/simo333/springinaction/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/simo333/springinaction/tacocloud/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.simo333.springinaction.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,21 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/design", "orders").access("hasRole('USER')")
+                .antMatchers("/", "**").access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and().build();
     }
 }
