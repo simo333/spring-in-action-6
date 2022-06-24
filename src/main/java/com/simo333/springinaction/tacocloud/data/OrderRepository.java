f65f2bd35f57d418b22a9986d027f30c0088ee9e
simo333 <szymon.333@wp.pl>
2022-06-24 17:35:28 +0200

    Minor fixes

--- src/main/java/com/simo333/springinaction/tacocloud/data/OrderRepository.java
+++ src/main/java/com/simo333/springinaction/tacocloud/data/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.simo333.springinaction.tacocloud.data;
 
+import com.simo333.springinaction.tacocloud.User;
 import com.simo333.springinaction.tacocloud.tacos.TacoOrder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface OrderRepository
         extends CrudRepository<TacoOrder, Long> {
 
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(
+            User user, Pageable pageable);
 }
--- src/main/java/com/simo333/springinaction/tacocloud/web/OrderController.java
+++ src/main/java/com/simo333/springinaction/tacocloud/web/OrderController.java
@@ -3,13 +3,13 @@ package com.simo333.springinaction.tacocloud.web;
 import com.simo333.springinaction.tacocloud.User;
 import com.simo333.springinaction.tacocloud.data.OrderRepository;
 import com.simo333.springinaction.tacocloud.tacos.TacoOrder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
@@ -21,12 +21,32 @@ public class OrderController {
 
     private final OrderRepository orderRepo;
 
-    public OrderController(OrderRepository orderRepo) {
+    private final OrderProps props;
+
+    public OrderController(OrderRepository orderRepo, OrderProps props) {
         this.orderRepo = orderRepo;
+        this.props = props;
     }
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute TacoOrder order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullName());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
+
         return "orderForm";
     }
 
@@ -43,6 +63,17 @@ public class OrderController {
         sessionStatus.setComplete();
 
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders",
+                orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
     }
 
 }
