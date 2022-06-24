dfbd46f2a89d53360578183c682e6ff0b1804441
simo333 <szymon.333@wp.pl>
2022-06-24 17:25:52 +0200

    Fixed warning (added final attribute)

--- src/main/java/com/simo333/springinaction/tacocloud/web/AdminController.java
+++ src/main/java/com/simo333/springinaction/tacocloud/web/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private OrderAdminService adminService;
+    private final OrderAdminService adminService;
 
     public AdminController(OrderAdminService adminService) {
         this.adminService = adminService;
